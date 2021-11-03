868ea1cdab46a54ac4b9c1476da2544a8df97874
Adish Gupta <adishgupta0@gmail.com>
2021-11-03 12:12:40 +0530

    deploy my react ap p

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React app</h1>
+      <p>Author : Telmo Sampaio</p>
+    </div>
+  );
+}
+
+export default App;
